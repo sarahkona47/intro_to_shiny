cb344576dec194a8f343f23eca4994ea338b1066
sarahkona47 <89939471+sarahkona47@users.noreply.github.com>
2022-03-29 14:30:15 -0500

    made app

--- app_files/basic_app_template.R
+++ app_files/basic_app_template.R
@@ -3,6 +3,9 @@
 # Beware! ... This alone will be a REALLY boring app. A blank page :(
 
 library(shiny)
-ui <- fluidPage()
+library(tidyverse)
+library(babynames)
+
+ui <- fluidPage("Sarah Choi")
 server <- function(input, output) {}
 shinyApp(ui = ui, server = server)
---
+++ babynames_app/app.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(tidyverse)
+library(babynames)
+
+ui <- fluidPage(textInput(inputId = "name", 
+                          label = "Name:", 
+                          value = "", 
+                          placeholder = "Enter Name Here"), 
+                sliderInput(inputId = "years", 
+                            label = "Years", 
+                            min = 1880, 
+                            max = 2017, 
+                            value = c(1880, 2017), 
+                            sep = ""),
+                selectInput(inputId = "sex",
+                            label = "Sex", 
+                            choices = c("Male" = "M", "Female" = "F")), 
+                submitButton(text = "Create Plot"), 
+                plotOutput(outputId = "timeplot"))
+
+server <- function(input, output) {
+  output$timeplot <- renderPlot(
+    babynames %>% 
+      filter(sex == input$sex,
+             name == input$name) %>% 
+      ggplot(aes(x = year, y = n)) +
+      geom_line() +
+      scale_x_continuous(limits = input$years)
+  )
+}
+shinyApp(ui = ui, server = server)
